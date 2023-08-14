c6b31f07b94844e9964b3370ea8c06d0013487eb
hotarudesign <hotarudesign1220@gmail.com>
2023-08-15 08:59:18 +0900

    🎉

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+
+
+
+<body>
+
+
+</body>
+<?php wp_footer(); ?>
+
+</html>
